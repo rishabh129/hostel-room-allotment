30e63e4edcc3624b0a63cde81624eb9ec276069a
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-01-27 23:45:02 +0530

    view pw
    updated view password

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import logo from './logo-bit.png';
-import { LoginFunc } from './login.js'
-import './App.css';
+import logo from "./logo-bit.png";
+import { LoginFunc } from "./login.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='headInfo'>
+      <div className="headInfo">
         <img src={logo} alt="logo" />
       </div>
-      <div class="background-slider">
-        
-    </div>
-      
-      <div className='bodyContent'>
+      <div class="background-slider"></div>
 
-        <div className='info'>
-
-          
-
-          <h2 className='heading'>Hostel Vision and Mission</h2>
+      <div className="bodyContent">
+        <div className="info">
+          <h2 className="heading">Hostel Vision and Mission</h2>
 
           <h4>Hostel Vision:</h4>
-          <p>To establish a welcoming and nurturing environment within the hostel that aligns seamlessly with the broader
-        goals of
-        the academic institution. Our vision is to create a globally recognized hostel facility that complements the
-        social,
-        academic, and personal growth of its residents.</p>
+          <p>
+            To establish a welcoming and nurturing environment within the hostel
+            that aligns seamlessly with the broader goals of the academic
+            institution. Our vision is to create a globally recognized hostel
+            facility that complements the social, academic, and personal growth
+            of its residents.
+          </p>
 
-    <h4>Hostel Mission:</h4>
-    <ol>
-        <li>Conducive and secure living space.</li>
-        <li>Cultivate a sense of community.</li>
-        <li>Contribute to holistic student development.</li>
-        <li>Prioritize safety and well-being.</li>
-        <li>Environmental responsibility.</li>
-    </ol>
-
+          <h4>Hostel Mission:</h4>
+          <ol>
+            <li>Conducive and secure living space.</li>
+            <li>Cultivate a sense of community.</li>
+            <li>Contribute to holistic student development.</li>
+            <li>Prioritize safety and well-being.</li>
+            <li>Environmental responsibility.</li>
+          </ol>
         </div>
 
-        <div className='form'>
-          {LoginFunc()}
-        </div>
-
+        <div className="form">{LoginFunc()}</div>
       </div>
-
-
     </div>
   );
 }
--- src/login.js
+++ src/login.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function LoginFunc() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       <h2 className="heading">Login</h2>
@@ -8,22 +14,32 @@ export function LoginFunc() {
       <div className="contain">
         <form action="/server">
           <div className="user">
-          
             <br />
             <input type="text" id="username" placeholder="Enter Username" />
           </div>
           <div className="pass">
-           
             <br />
-            <input type="password" placeholder="Enter Password" id="password" />
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter password"
+              id="password"
+            />
+            <div className="eye-icon" onClick={togglePasswordVisibility}>
+            {showPassword
+                ? "🔓"
+                : '🔒'}
+            </div>
+            <br />
           </div>
           <div className="loginbutton">
             <button className="login">Login</button>
           </div>
         </form>
       </div>
-      <div >
-        <a href="/forget.html" className="passforget">Forget password?</a>
+      <div>
+        <a href="/forget.html" className="passforget">
+          Forget password?
+        </a>
       </div>
     </div>
   );
