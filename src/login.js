e3b1b14e6626d228ec4abf3393d0b9835883df7f
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-02-06 15:47:37 +0530

    eye-icon

--- src/login.js
+++ src/login.js
@@ -15,30 +15,40 @@ export function LoginFunc() {
         <form action="/server">
           <div className="user">
             <br />
-            <input type="text" id="username" placeholder="Enter Username" required/>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter Username"
+              required
+            />
           </div>
           <div className="pass">
             <br />
             <input
-              type={showPassword ? "text" : "password" }
+              type={showPassword ? "text" : "password"}
               placeholder="Enter password"
               id="password"
-            required />
+              required
+            />
             <div className="eye-icon" onClick={togglePasswordVisibility}>
-            {showPassword
-                ? "🔓"
-                : '🔒'}
+              {showPassword ? (
+                <i className="fas fa-eye" style={{ color: 'black' }}></i>
+              ) : (
+                <i className="fas fa-eye-slash" style={{ color: 'black' }}></i>
+              )}
             </div>
+
             <br />
           </div>
           <div className="loginbutton">
             <button className="login">Login</button>
+            <button className="signup">Sign Up</button>
           </div>
         </form>
       </div>
       <br />
       <div>
-        <a href="/forget.html" className="passforget">
+        <a href="#" className="passforget">
           Forget password?
         </a>
       </div>
