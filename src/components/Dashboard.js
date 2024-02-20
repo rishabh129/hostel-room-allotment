0cf3eee313ba176e30c27ef3ef6758cf12915a93
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-02-20 15:44:30 +0530

    commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,34 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { database } from './firebase';
-import { useHistory } from 'react-router-dom';
-//pushCheck
+import { signOut } from "firebase/auth";
+import React from "react";
+import { database } from "./firebase";
+import { useHistory } from "react-router-dom";
+import "./styles.css";
+import logo from "../images/BIT-logo.png";
+
 const Dashboard = () => {
-  const history = useHistory()
-  const handleSignOut=()=>{
-    signOut(database).then(val=>{
-      console.log("Logged out! See you later")
-      history.push('/')
-    })
-  }
-  
+  const history = useHistory();
+  const handleSignOut = () => {
+    signOut(database).then((val) => {
+      console.log("Logged out! See you later");
+      history.push("/");
+    });
+  };
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
-      <p> Hostel Room Allocation </p>
-      <button onClick={handleSignOut}>Sign Out</button>
+      <nav className="navbar">
+        <div className="navbar__background"></div>
+        <div className="navbar__content">
+          <div className="navbar__logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className="navbar__brand">BIRLA INSTITUTE OF TECHNOLOGY</div>
+          <div class="navbar__links">
+            <button onClick={handleSignOut} className="navbar__link">Sign Out</button>
+
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
