6c87d16f970e6a7d5290a4b7eef46115e100127b
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-02-23 23:01:08 +0530

    student admin radio ui

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,34 +1,13 @@
 import React from "react";
 import "./styles.css";
-import logo from "../images/BIT-logo.png";
 import Navbar from "./Navbar";
 
 const Dashboard = () => {
-  // const history = useHistory();
-  // const handleSignOut = () => {
-  //   signOut(database).then((val) => {
-  //     console.log("Logged out! See you later");
-  //     history.push("/");
-  //   });
-  // };
 
   return (
-    // <div>
-    //   <nav className="navbar">
-    //     <div className="navbar__content">
-    //       <div className="navbar__logo">
-    //         <img src={logo} alt="Logo" />
-    //       </div>
-    //       <div className="navbar__brand">BIRLA INSTITUTE OF TECHNOLOGY</div>
-    //       <div class="navbar__links">
-    //         <span onClick={handleSignOut} className="navbar__link">Sign Out</span>
-            
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
-
     <Navbar loggedIn={true} />
+
+    
   );
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { css } from "@emotion/react";
 import { useHistory, Link, useNavigate } from "react-router-dom";
 import "./styles.css"; // Import CSS file
@@ -11,33 +11,30 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 
-
 const LoginPage = () => {
-
   const [loading, setLoading] = useState(false);
 
   const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-  z-index: 1200;
-`;
-  
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    z-index: 1200;
+  `;
+
   const overlayStyle = {
-    position: 'fixed',
+    position: "fixed",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity here
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust the opacity here
     zIndex: 9999, // Make sure the z-index is higher than other content
-    display: loading ? 'flex' : 'none',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: loading ? "flex" : "none",
+    justifyContent: "center",
+    alignItems: "center",
   };
 
-  
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
@@ -78,14 +75,17 @@ const LoginPage = () => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
-  const isLoggedIn = false;
+
+  const [selectedOption, setSelectedOption] = useState("option1");
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
     <main className="background_wrapper">
-      {/* {Navbar(loggedIn={isLoggedIn})} */}
-
       <Navbar loggedIn={false} />
-      
+
       <div style={overlayStyle}>
         <FadeLoader
           color={"#000000"}
@@ -113,7 +113,42 @@ const LoginPage = () => {
               SIGN UP
             </div>
           </div>
+
           <div className="login_form_content">
+            {login === false ? (
+              <div className="login_content_box">
+                <ion-icon name="person-outline"></ion-icon>
+                <div className="login_content_box--input">
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Full Name"
+                    className="login__input name--input"
+                    // value={name}
+                    // onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+            ) : null}
+
+            {login === false ? (
+              <div className="login_content_box">
+                <ion-icon name="person-outline"></ion-icon>
+                <div className="login_content_box--input">
+                  <input
+                    name="roll"
+                    type="text"
+                    placeholder="Roll Number"
+                    className="login__input roll--input"
+                    // value={name}
+                    // onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+            ) : null}
+
             <div className="login_content_box">
               <ion-icon name="person-outline"></ion-icon>
               <div className="login_content_box--input">
@@ -157,6 +192,38 @@ const LoginPage = () => {
                 </div>
               </div>
             </div>
+
+            {login === true ? (
+              <div className="login_content_box">
+                <ion-icon name="person-outline"></ion-icon>
+                <div className="radio-switch">
+                  <input
+                    type="radio"
+                    id="radio1"
+                    name="radioswitch"
+                    value="student"
+                    checked={selectedOption === "student"}
+                    onChange={handleOptionChange}
+                    className="radio-switch-input" 
+                    
+                  />
+                  <label htmlFor="radio1" className="radio-switch-label">Student</label>
+
+                  <input
+                    type="radio"
+                    id="radio2"
+                    name="radioswitch"
+                    value="admin"
+                    checked={selectedOption === "admin"}
+                    onChange={handleOptionChange}
+                    className="radio-switch-input"
+                  />
+                  <label htmlFor="radio2" className="radio-switch-label">Admin</label>
+                  <div class="slide-indicator"></div>
+                </div>
+              </div>
+            ) : null}
+
             <button className="button__login">
               {login ? "Sign-In" : "Sign-Up"}
             </button>
