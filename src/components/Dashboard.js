2271ff0e2a504f3b3b34aff7bd52d437ea9951db
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-02-24 14:37:10 +0530

    dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,39 @@
 import React from "react";
-import "./styles.css";
+import "./dashboardStyle.css";
 import Navbar from "./Navbar";
 
 const Dashboard = () => {
-
   return (
-    <Navbar loggedIn={true} />
+    <div className="dashboardBody">
+      <Navbar loggedIn={true} />
 
-    
+      <div className="Container">
+        <div className="menuBar">
+          <h1>Menu Bar</h1>
+          
+        </div>
+        <div className="bodyContent">
+          <div className="sideMenu">
+            <h2>Side Menu</h2>
+            <ul className="menuItems">
+              <li className="menuItem">Dashboard</li>
+              <li className="menuItem">Profile</li>
+              <li className="menuItem">Application Form</li>
+              <li className="menuItem">Track Application Status</li>
+              <li className="menuItem">Contact</li>
+            </ul>
+          </div>
+          <div className="mainContent">
+            <h2>Main Content</h2>
+            
+          </div>
+          <div className="noticeSection">
+            <h2>Notice Section</h2>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
