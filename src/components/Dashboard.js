756229b2c95f09b460c600bb28df337948141eca
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-02-24 20:05:58 +0530

    sideMenu

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./dashboardStyle.css";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -13,13 +14,49 @@ const Dashboard = () => {
         </div>
 
         <div className="bodyContent">
-          
           <div className="sideMenu">
             <h1>Side Menu</h1>
+            <ul className="menuItems">
+              <li className="menuItem">
+              <Link to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="menuItem">
+              <Link to="/dashboard">
+                  Profile
+                </Link>
+              </li>
+              <li className="menuItem">
+              <Link to="/dashboard">
+                  Application Form
+                </Link>
+              </li>
+              <li className="menuItem">
+              <Link to="/dashboard">
+                  Track Application
+                </Link>
+              </li>
+              <li className="menuItem">
+              <Link to="/dashboard">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
           </div>
+
           <div className="mainContent">
             <h1>Main Content</h1>
+            <div className="applicationFormButton">
+              <h3>Application Form</h3>
+            </div>
+
+            <div className="trackApplicationButton">
+              <h3>Track Application Status</h3>
+            </div>
+
           </div>
+
           <div className="noticeSection">
             <h1>Notice</h1>
           </div>
