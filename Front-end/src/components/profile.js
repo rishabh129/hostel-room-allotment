9b9d71ba923cc325749440d37a51156dfc902d0f
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2024-03-28 23:15:40 +0530

    profile

---
+++ Front-end/src/components/profile.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './profile.css'; // Import CSS file
+
+function Profile() {
+    return (
+        <div className="main">
+            <div className="sidemenu">
+                <UpperInfo />
+                <LowerInfo />
+            </div>
+            <div className="maincontent">
+                <div className="welcome">
+                    Welcome back! You are logged in as: Name Here
+                </div>
+                <ProfileTable />
+            </div>
+        </div>
+    );
+}
+
+function UpperInfo() {
+    return (
+        <div className="upperinfo">
+            <img src="download.jpeg" alt="" />
+            <h3>Name here</h3>
+        </div>
+    );
+}
+
+function LowerInfo() {
+    return (
+        <div className="lowerinfo">
+            <div className="info-item">
+                <span className="label">Roll Number:</span>
+                <span className="value">12345</span>
+            </div>
+            <div className="info-item">
+                <span className="label">Department:</span>
+                <span className="value">Computer Science</span>
+            </div>
+            <div className="info-item">
+                <span className="label">Course:</span>
+                <span className="value">Bachelor of Technology</span>
+            </div>
+            <div className="info-item">
+                <span className="label">Hostel Number:</span>
+                <span className="value">H1</span>
+            </div>
+            <div className="info-item">
+                <span className="label">Room Number:</span>
+                <span className="value">101</span>
+            </div>
+        </div>
+    );
+}
+
+function ProfileTable() {
+    return (
+        <table className="table table-bordered">
+            <thead>
+                <tr id="profile_row">
+                    <th>Hostel Plan</th>
+                    <th>Room Type</th>
+                    <th>Building Name</th>
+                    <th>Floor Name</th>
+                    <th>Room Name</th>
+                    <th>Allocated On</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colSpan="6">Data Not Found...</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default Profile;
