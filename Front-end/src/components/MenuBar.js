bff97a4b5f89632ca7245561d22b484133718b76
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2024-03-30 22:57:13 +0530

    temp push

--- Front-end/src/components/MenuBar.js
+++ Front-end/src/components/MenuBar.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./MenuBarStyles.css";
 
 const MenuBar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
-
-
     <nav className="menuBar">
       <NavLink exact to="/dashboard" activeClassName="active" className="nav-link">
         Dashboard
@@ -13,18 +17,42 @@ const MenuBar = () => {
       <NavLink to="/profile" activeClassName="active" className="nav-link">
         Profile
       </NavLink>
-      <NavLink
-        to="/application"
-        activeClassName="active"
-        className="nav-link"
-      >
-        Application Form
-      </NavLink>
-      <NavLink
-        to="/application-status"
-        activeClassName="active"
-        className="nav-link"
-      >
+      <div className="dropdown">
+        <NavLink
+          to="/application"
+          activeClassName="active"
+          className="nav-link"
+          onClick={toggleDropdown}
+        >
+          Application Form
+        </NavLink>
+        {showDropdown && (
+          <div className="dropdown-content">
+            <NavLink
+              to="/new-room-application"
+              activeClassName="active"
+              className="dropdown-link"
+            >
+              New Room Application
+            </NavLink>
+            <NavLink
+              to="/swap-room-application"
+              activeClassName="active"
+              className="dropdown-link"
+            >
+              Swap Room Application
+            </NavLink>
+            <NavLink
+              to="/vacate-room-application"
+              activeClassName="active"
+              className="dropdown-link"
+            >
+              Vacate Room Application
+            </NavLink>
+          </div>
+        )}
+      </div>
+      <NavLink to="/application-status" activeClassName="active" className="nav-link">
         Application Status
       </NavLink>
       <NavLink to="/contact" activeClassName="active" className="nav-link">
