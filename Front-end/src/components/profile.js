cfdbf8cf3277028f21336b637c88e5b40028721a
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2024-03-31 10:41:40 +0530

    Revert "table"
    This reverts commit ac5819093739a9b25199e0b4c0bbaccce3b0e2b4.

--- Front-end/src/components/profile.js
+++ Front-end/src/components/profile.js
@@ -1,89 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./profile.css";
 import "./dashboardStyle.css";
 import Navbar from "./Navbar";
 import MenuBar from "./MenuBar";
 
 const ProfilePage = () => {
-  // Define state variable to hold the profile data
-  const [profileData, setProfileData] = useState([]);
-
-  // Fetch profile data from backend API
-  useEffect(() => {
-    // Make a fetch request to your backend API to get profile data
-    fetch("your-backend-api-url")
-      .then((response) => response.json())
-      .then((data) => setProfileData(data))
-      .catch((error) => console.error("Error fetching profile data: ", error));
-  }, []); // This effect runs once after the initial render
-
   return (
     <div className="dashboardBody">
       <Navbar loggedIn={true} />
       <div className="Container">
         <MenuBar />
-        <Profile profileData={profileData} />
+        <Profile />
       </div>
     </div>
   );
 };
 
-function Profile({ profileData }) {
+function Profile() {
   return (
     <div className="main">
       <div className="sidemenu">
-        <UpperInfo profileData={profileData} />
-        <LowerInfo profileData={profileData} />
+        <UpperInfo />
+        <LowerInfo />
       </div>
       <div className="maincontent">
         <div className="welcome">
           <p>Welcome back! You are logged in as: <em>Dr. K. S. Patnaik</em></p>
         </div>
-        <ProfileTable profileData={profileData} />
+        <ProfileTable />
       </div>
     </div>
   );
 }
 
-function UpperInfo({ profileData }) {
+function UpperInfo() {
   return (
     <div className="upperinfo">
       <img src="download.jpeg" alt="" />
-      <h3>{profileData.name}</h3>
+      <h3>Name here</h3>
     </div>
   );
 }
 
-function LowerInfo({ profileData }) {
+function LowerInfo() {
   return (
     <div className="lowerinfo">
       <div className="info-item">
         <span className="label">Roll Number:</span>
-        <span className="value">{profileData.rollNumber}</span>
+        <span className="value">12345</span>
       </div>
       <div className="info-item">
         <span className="label">Department:</span>
-        <span className="value">{profileData.department}</span>
+        <span className="value">Computer Science</span>
       </div>
       <div className="info-item">
         <span className="label">Course:</span>
-        <span className="value">{profileData.course}</span>
+        <span className="value">Bachelor of Technology</span>
       </div>
       <div className="info-item">
         <span className="label">Hostel Number:</span>
-        <span className="value">{profileData.hostelNumber}</span>
+        <span className="value">H1</span>
       </div>
       <div className="info-item">
         <span className="label">Room Number:</span>
-        <span className="value">{profileData.roomNumber}</span>
+        <span className="value">101</span>
       </div>
     </div>
   );
 }
 
-function ProfileTable({ profileData }) {
+function ProfileTable() {
   return (
-    
     <table className="table table-bordered">
       <thead>
         <tr id="profile_row">
@@ -97,18 +84,10 @@ function ProfileTable({ profileData }) {
       </thead>
       <tbody>
         <tr>
-          {/* Display profile data in the table */}
-          <td>{profileData.hostelPlan}</td>
-          <td>{profileData.roomType}</td>
-          <td>{profileData.buildingName}</td>
-          <td>{profileData.floorName}</td>
-          <td>{profileData.roomName}</td>
-          <td>{profileData.allocatedOn}</td>
+          <td colSpan="6">Data Not Found...</td>
         </tr>
       </tbody>
     </table>
-
-    
   );
 }
 
