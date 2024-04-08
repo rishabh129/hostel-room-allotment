d04ddd6d89783c33310c7d8e4bed582a6e812ac1
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-04-08 22:56:36 +0530

    Revert "as"
    This reverts commit 3f2008488d88f3ea9b9155f7831dd7d715292cc5.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import ApplicationPage from './components/ApplicationPage';
 import Profile from './components/profile'; 
 import ContactForm from './components/contactus';
 import adminDashboard from './components/adminDashboard';
-import Swaproom from  './components/swaproom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={(props) => (
@@ -24,7 +23,6 @@ const App = () => {
           <PrivateRoute path="/dashboard" component={Dashboard} />
           <Route path="/about" component={About} /> 
           <Route path="/application" component={ApplicationPage} />
-          <Route path="/swaproom" component={Swaproom} />
           <Route path="/profile" component={Profile} />
           <Route path="/contactus" component={ContactForm} />
           <Route path="/admin" component={adminDashboard} />
