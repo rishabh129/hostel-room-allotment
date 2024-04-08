e615c9883930b34991185a3e9df272843b07341a
Rishi Raj <106508188+Rishi1808@users.noreply.github.com>
2024-04-08 20:14:56 +0530

    admin full

--- src/components/adminDashboard.js
+++ src/components/adminDashboard.js
@@ -3,12 +3,14 @@ import Navbar from './Navbar';
 import "./admin.css";
 
 const AdminDashboard = () => {
-  return (
+  return ( 
+
+    <div className="admin-main">
+       <Navbar></Navbar>
+       <h3 class="welcome">Welcome, </h3>
+
         <div className="container-admin">
-          <div className="nav">
-            <Navbar></Navbar>
-          </div>
-      
+       <h3 >Application Usage Statistics</h3>
       <div className="dashboard-card">
         <h2>Applications</h2>
         <div className="pie-chart" id="application-chart"></div>
@@ -18,12 +20,14 @@ const AdminDashboard = () => {
         <h2>Rooms</h2>
         <div className="pie-chart" id="room-chart"></div>
       </div>
-
-      <div className="admin-work">
-        <button className="big-button">Application List</button>
-        <button className="big-button">List of Rooms</button>
       </div>
-    </div>
+    <div className="admin-work">
+        <h2> 1. List of Student application:-</h2><button className="big-button">Application List</button>
+        <br />
+        <br />
+        <h2>2.List of Rooms in hostel 5:-</h2><button className="big-button">List of Rooms</button>
+      </div>
+  </div>
   );
 };
 
